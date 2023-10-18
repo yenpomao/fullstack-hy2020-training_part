b71396496c4c267e506089de7163ed560954f442
yenpomao <yenpomao@gmail.com>
2023-10-18 14:17:41 +0200

    second update

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,55 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Note from './components/Note'
 import './App.css'
 
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
   )
-}
-
-
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
-const App = () => {
-  const [value, setValue] = useState(10)
-
-  const hello = (who) => {
-    const handler = () => {
-      console.log('hello', who)
+  const [showAll, setShowAll] = useState(true)
+  
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
     }
-    return handler
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
+  
+  const handleNoteChange = (event) => {
+    // console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
 
   return (
     <div>
-      {value}
-      <button onClick={hello('world')}>button</button>
-      <button onClick={hello('react')}>button</button>
-      <button onClick={hello('function')}>button</button>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -9,4 +9,25 @@ import './index.css'
 //   </React.StrictMode>,
 // )
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+
+const notes = [
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only JavaScript',
+      important: false
+    },
+    {
+      id: 3,
+      content: 'GET and POST are the most important methods of HTTP protocol',
+      important: true
+    }
+  ]
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<App notes={notes}/>)
